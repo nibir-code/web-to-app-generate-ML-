ee14929f6f20805185af9836bb89d0f362790954
nibir-code <mubtashimfuadnibir@gmail.com>
2021-12-29 22:00:53 +0600

    first ever  commit

---
+++ RestimageToDatabasev2/webtoapp/webtoapp/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+
+class insertbutton(models.Model):
+    buttonColor = models.CharField(max_length= 60)
+
+
+class buttoninfo(models.Model):
+    Button_Number = models.IntegerField()
+    Button_letter = models.CharField(max_length=80)
+    Button_height = models.IntegerField()
+    Button_width = models.IntegerField()
+    Button_Starting_xpoint = models.IntegerField()
+    Button_Starting_ypoint = models.IntegerField()
